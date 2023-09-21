8ba0fb60a9d61a0b36591266cdf5ef0ee2b9bf82
Sharjeel Khan <sharjeelaslam96@gmail.com>
2023-09-21 15:05:11 +0500

    Favicon update and title change
    Titlc change to this.
    Bookings - Suja Driving School

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/favicon-suja.png';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Suja Driving School Form
+          Bookings - Suja Driving School
         </a>
       </header>
     </div>
